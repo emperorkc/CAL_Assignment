cc442d7864b5c9341d7ab13ca132b89893c1bdb4
emperorkc <kevin.reinholtz@hotmail.com>
2014-09-18 14:32:47 +0200

    Struct Implemented

---
+++ studentNode.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<student.h>
+
+typedef struct studentNode *StudentNodeTypePtr;
+typedef struct studentNode
+{
+StudentType* student;
+StudentNodeTypePtr next;
+
+}StudentNodeType;
