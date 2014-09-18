df2df48ebc9807a04e6d92f5daeb2b035e3bd9a7
nephius <nephius1337@gmail.com>
2014-09-18 14:11:57 +0200

    defined struct

---
+++ teacher.h
@@ -0,0 +1,7 @@
+#include "courseNode.h"
+
+typedef struct teacherNode {
+	char* name;
+	int ID;
+	courseNode course;
+} teacherType;
---
+++ teacherNode.h
@@ -0,0 +1,7 @@
+#include "teacher.h"
+typedef struct teacherNode* teacherNodePtr;
+
+typedef struct teacherNode {
+	teacherType teacher;
+	teacherNodePtr next;
+}teacherNodeType;
